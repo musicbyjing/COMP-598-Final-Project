4bebf324f5eae963d59334d786c45e402afbd083
musicbyjing <musicbyjing@gmail.com>
2020-11-25 21:02:21 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# COMP-598-Final-Project
+
+```
+Your team has been hired by a not-for-profit that wants to understand candidate discussion in the days following the US election. They have indicated that they are especially concerned with perceptions of election legitimacy. Specifically, they want to know:
+1. The salient topics discussed around each candidate and what each topic primarily concerned
+2. Relative engagement with those topics among liberals and conservatives
+```
+
+I've taken posts from the categories Hot, Top (past day), and Controversial (past day). See [here](https://www.reddit.com/r/explainlikeimfive/comments/1rqjwp/eli5_what_does_top_new_hot_controversial_old_mean/) for an explanation. We'll have to justify our design choices in the report, so we'll have to decide which category to use for our 2000 posts.
---
+++ collect_and_filter.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import os
+from datetime import datetime
+
+# CONSTANTS
+num_posts = 333
+subs = ['conservative'] # 'politics', 'conservative'
+categories = ['hot'] # 'hot', 'top', 'controversial'
+output_dir = 'data'
+current_time = datetime.now().strftime("%Y-%m-%d")
+
+# then conservative hot
+
+"""
+Pull data from Reddit API, given:
+    subreddit
+    category (e.g. Top)
+    output file
+    filter function
+    output file for filtered data
+    after marker
+Returns the next after
+"""
+def hit_api(sub_name, category, output_file, filter_func, filtered_output, after):
+    data = requests.get(f'https://api.reddit.com/r/{sub_name}/{category}/?t=day&limit=100&after={after}', 
+        headers={'User-Agent': 'macos: requests (by /u/school_reddit_acc)'})
+    content = data.json()['data']
+    posts = content['children'] # type list
+    with open(output_file, 'a') as f: # append each post to the output file
+        with open(filtered_output, 'a') as g:
+            for post in posts:
+                data = post['data']
+                json.dump(data, f)
+                f.write('\n')
+                if(filter_func(data)):
+                    json.dump(data, g)
+                    g.write('\n')
+
+    return content['after'] # for the next iteration
+
+"""
+Input: post in dictionary form
+Output: True if post mentions Biden or Trump, case-insensitive, False otherwise
+"""
+def mentions(data):
+    title = data['title']
+    return True if 'trump' in title.lower() or 'biden' in title.lower() else False
+
+
+def main():
+    after = ""
+    for i in range(num_posts//100 + 1):
+        # iteration i uses `after` returned in iteration i-1
+        for sub_name in subs:
+            for category in categories:
+                output_file = os.path.join(os.getcwd(), output_dir, f'{current_time}_{sub_name}_{category}.json')
+                filtered_output = os.path.join(os.getcwd(), output_dir, f'{current_time}_{sub_name}_{category}_filtered.json')
+                after = hit_api(sub_name, category, output_file, mentions, filtered_output, after)
+
+if __name__ == '__main__':
+    main()
